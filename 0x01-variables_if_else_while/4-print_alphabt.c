ca7941883a8bee01fca4effc448977c205e088cd
Mohannad El-Sayeh <eng.mohannadelsayeh@gmail.com>
2023-02-18 08:24:56 -0800

    Aphab~(e)t

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - The Starting point of the program
+ *
+ * Return: Ends the program with code 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c == 'q' || c == 'e')
+		{
+			c++;
+			continue;
+		}
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
