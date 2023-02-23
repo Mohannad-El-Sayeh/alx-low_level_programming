0ece544ee46b05b1b9848438a4bf039137f8a1ce
Mohannad El-Sayeh <eng.mohannadelsayeh@gmail.com>
2023-02-22 17:35:19 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Gets the absolute value of an integer.
+ *
+ * @i: The number you want to get its absolute.
+ *
+ * Return: Absolute value of i.
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		return (-i);
+	}
+	return (i);
+}
